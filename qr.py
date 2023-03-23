8718697ac9a9d7dad82d80fb4d0ae8497c146a8a
lxopencv <102907852+lxopencv@users.noreply.github.com>
2023-03-23 09:03:14 +0800

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,13 @@
+import cv2
+
+# Define the dictionary
+dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+
+# Generate the marker
+marker = cv2.aruco.drawMarker(dictionary, 23, 200)
+
+# Resize the marker to 25mm x 100mm
+marker = cv2.resize(marker, (25, 25))
+
+# Save the marker as qr.png
+cv2.imwrite('qr.png', marker)
